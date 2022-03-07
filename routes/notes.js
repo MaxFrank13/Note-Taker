c5993291674957e04a32aece4828255e55651e90
Max <maxfrank13@gmail.com>
2022-03-07 18:19:06 -0500

    added endpoint and routes for notes page

---
+++ routes/notes.js
@@ -0,0 +1,23 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+notes.get('/', (req, res) =>
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+)
+
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4()
+        }
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Added note`);
+    }
+})
+
+module.exports = notes;
